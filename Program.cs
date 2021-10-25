7cf06f13d1a59e70c6a4e2a331cdab62493fee41
Vitória Cristiana <vitoriacristiana290@gmail.com>
2021-10-25 12:39:01 -0300

    resolução exercício

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Bhaskara
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double  a, b, c;
+            double  x1 , x2 , delta;
+
+            Console.WriteLine("Bem vindo ao cálculo de Bharkara.\n ");
+
+            Console.Write("Informe o valor de a:");
+             a= Convert.ToDouble (Console.ReadLine());
+
+             Console.Write("Informe o valor de b:");
+             b= Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Informe o valor de c:");
+            c= Convert.ToDouble(Console.ReadLine());
+
+            if (a == 0)
+            {
+            Console.WriteLine(" \n Não é uma equação de segundo grau!");
+             return;
+            }
+
+            delta = ( b * b ) - 4 * a * c;
+
+            if (delta < 0)
+            {   
+             Console.WriteLine($" \n Como delta = {delta} é menor que 0, a equação não possui raízes reais!");
+            return;
+            }
+            x1 = ((-b + Math.Sqrt (delta)) / (2 * 1));
+            x2 = ((-b - Math.Sqrt (delta)) / (2 * 1));
+
+            Console.WriteLine ($"\n x1 = {x1:N2} e x2 = {x2:N2}");
+
+            
+
+
+
+
+
+            
+
+        }
+    }
+}
